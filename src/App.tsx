7466e696a3385250d17829f563f3cef37c14cc81
Araya <joaoaraya7@gmail.com>
2021-10-02 20:37:27 -0300

    evenos para criação de salas no banco de dados do firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+// Importar dependência para o uso das Rotas
+// OBS1: o Switch nunca deixará chamar 2 rotas ao mesmo tempo (apenas uma por vez)
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+// Importar Páginas
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
+// Importar contextos de autenticação do Firebase
 import { AuthContextProvider } from './contexts/AuthContext'
 
-/// Ação principal
+/// Ação Principal e Rotas
+// OBS 1: ao usar o exact, o local só séra acessado se for exatamente o mesmo do path
+// OBS 2: ao usar :id vc pode usar qualquer coisa escrita após '/rooms/....'
+
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path='/' exact component={Home} />
-        <Route path='/rooms/new' component={NewRoom} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/rooms/new' component={NewRoom} />
+          <Route path='/rooms/:id' component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,7 @@
-//import { useAuth } from '../hooks/useAuth';
-import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth';
+import { Link, useHistory } from 'react-router-dom'
+
+import { FormEvent, useState } from 'react'
 
 /// No React é preciso Importar as imagens antes de colocar os links
 import ilustrationImg from '../assets/images/illustration.svg';
@@ -7,6 +9,7 @@ import logoImg from '../assets/images/logo.svg';
 
 // Importar componentes..
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 
 // Importar o estilo geral da página
 import '../styles/auth.scss';
@@ -17,7 +20,32 @@ import '../styles/auth.scss';
 // Obs4: as tags usadas em letras Maiúsculas, são: componentes
 
 export function NewRoom() {
-    //const { user } = useAuth();
+    const { user } = useAuth();
+    const history = useHistory();
+
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        // não permitir muitos espaço e nem espaço vazio
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        // Criar seção no banco de dados com o nome 'Rooms'
+        const roomRef = database.ref('rooms');
+
+        // Se der certo(await) incluir(push) os dados (title = $newRoom(nome da room na input)
+        // e authorId = (id do usuario logado)
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        });
+
+        // Após criar a sala no Banco de dados, redirecionar para a pagina da sala
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return (
         <div id='page-auth'>
@@ -30,8 +58,13 @@ export function NewRoom() {
                 <div className='main-content'>
                     <img src={logoImg} />
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" placeholder="Nome da sala" />
+                    <form onSubmit={handleCreateRoom}>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
+                        />
                         <Button type="submit"> Criar sala </Button>
                     </form>
                     <p>
