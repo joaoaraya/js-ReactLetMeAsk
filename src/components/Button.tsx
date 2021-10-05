8bac0dce44592a288a9fd4ce61fc89a132f34b7d
Araya <joaoaraya7@gmail.com>
2021-10-05 17:29:35 -0300

    pagina de admin criada

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,11 +5,17 @@ import { ButtonHTMLAttributes } from 'react'
 import '../styles/button.scss'
 
 // Define a propriedade
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean;
+};
 
 // Exportar o botão em forma de componente (use os '...' para incluir todas os atributos vindo das propriedades)
-export function Button(props: ButtonProps) {
+// OBS 1: isOulined é opcional nos botoes, caso nao for usado o padrão é = false
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
     return (
-        <button className="button" {...props} />
+        <button
+            className={`button ${isOutlined ? 'outlined' : ''}`}
+            {...props}
+        />
     )
 }
---
+++ src/hooks/useRooms.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { database } from "../services/firebase";
+
+/// tipos
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+}>
+
+type QuestionType = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+}
+
+export function useRoom(roomId: string) {
+    const [questions, setQuestions] = useState<QuestionType[]>([])
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`);
+
+        /// Ouvir alterações de dados do firebase (once = 1x), (on = sempre)
+        roomRef.on('value', room => {
+            const databaseRoom = room.val();
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighlighted: value.isHighlighted,
+                    isAnswered: value.isAnswered,
+                }
+            })
+
+            setTitle(databaseRoom.title);
+            setQuestions(parsedQuestions);
+        })
+    }, [roomId]); // [] os dados sao recarregados se nudar a url
+
+    return { questions, title }
+}
